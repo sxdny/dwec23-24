0dfd0632399774426391b816644fb2319d795318
SIDNEY SILVA <67187745+sxdny@users.noreply.github.com>
2023-11-28 17:28:36 +0100

    acabar el juego del ahorcado

--- actividades/ahorcado/script.js
+++ actividades/ahorcado/script.js
@@ -2,18 +2,21 @@
 
 const app = document.getElementById('app');
 
+const teclado = document.getElementById('teclado');
+
 const titulo = document.getElementsByClassName('titulo');
 const palabra = document.getElementsByClassName('palabra');
+
+const erroresDOM = document.getElementById('errores');
+const intentosDOM = document.getElementById('intentos');
+
 const wordToGuessDOM = document.getElementById('wordToGuess');
+
 
 // array de botones del documento
 const botones = document.getElementsByClassName('letra');
 
 let opcUsuario = 0;
-
-let matrizPalabras = [
-
-]
 
 let arrPalabras = [
 
@@ -39,24 +42,100 @@ let arrPalabras = [
     }
 ];
 
-// seleccionar una palabra random después que el usuaroi haya elegido el tipo de palabra que quiere.
+// seleccionar una palabra random después que el usuario haya elegido el tipo de palabra que quiere.
 
 let wordToGuess = arrPalabras[opcUsuario].palabras[Math.floor(Math.random() * arrPalabras[opcUsuario].palabras.length)];
 
-wordToGuessDOM.innerText = wordToGuess;
+// convertir la palabra a array
+wordToGuess = wordToGuess.split("");
 
-console.log(Math.floor(Math.random() * arrPalabras.length));
+// este split es simplementa para mostrar bien la palabra
+// mostrar la palabra en el DOM
+wordToGuess.forEach(() => {
+    wordToGuessDOM.innerHTML += `<li>?</li>`;
+});
+
+let intentos = 0;
+let errores = 0;
 
 // hay que iterar los botones, porqué hay varios
 for (let i = 0; i < botones.length; i++) {
+   
+    botones[i].addEventListener("click", (e) => {
+        checkLetter(e.target.innerHTML.toLowerCase());
+
+        if (checkLetter(e.target.innerHTML)) {
+            e.target.classList.add("letra-correcta");
+            // deshabilitar el botón
+            e.target.setAttribute("disabled", "disabled");
+        }
+        else {
+            errores++;
+            erroresDOM.innerHTML = errores;
+            e.target.classList.add("letra-incorrecta");
+            // deshabilitar el botón
+            e.target.setAttribute("disabled", "disabled");
+        }
+
+        
+        intentos++;
+        intentosDOM.innerHTML = intentos;
+        
+        if(checkWord()) {
+            console.log("Has ganado!");
+            console.log(teclado);
+
+        }
+    });
+
     botones[i].addEventListener("click", (e) => {
         if (
             e.target.classList.contains("letra") &&
             !e.target.classList.contains("seleccionada")
         ) {
             e.target.classList.toggle("seleccionada");
-            // tenemos el "valor de las letras"
-            console.log(e.target.innerHTML);
+            
         }
     });
+}
+
+// función que comprueba si la letra está en la palabra
+function checkLetter(letter) {
+    if (wordToGuess.includes(letter.toLowerCase()) || wordToGuess.includes(letter.toUpperCase())) {
+        console.log("Letra correcta.");
+        // mostrar por pantalla
+        // recorrer el array de la palabra
+        wordToGuess.forEach((letraAdivinar, index) => {
+            if (letraAdivinar.toLowerCase() === letter.toLowerCase()) {
+                // mostrar la letra en el DOM
+                wordToGuessDOM.children[index].innerHTML = letraAdivinar;
+            }
+        });
+        return true;
+    }
+    else {
+        console.log("Letra incorrecta.");
+        return false;
+    }
+}
+
+// función que comprueba si la palabra está completa
+function checkWord() {
+    // recorrer el array de la palabra
+    let palabraCompleta = true;
+    
+    let palabrasDOM = wordToGuessDOM.children;
+
+    // solo se puede iterar asi?
+    for (let i = 0; i < palabrasDOM.length; i++) {
+        if (palabrasDOM[i].innerHTML === "?") {
+            palabraCompleta = false;
+            
+        }
+    }
+    
+    if (palabraCompleta) {
+        console.log("Palabra completa.");
+        teclado.classList.add("d-none");
+    }
 }
