0df68c5150be85cfd9825da6c9b4227a21ac9217
SIDNEY SILVA <67187745+sxdny@users.noreply.github.com>
2023-11-29 21:15:48 +0100

    Acabar ejercicio Ahorcado
    mirar el contenido de la actividad

--- actividades/ahorcado/script.js
+++ actividades/ahorcado/script.js
@@ -7,10 +7,20 @@ const teclado = document.getElementById('teclado');
 const titulo = document.getElementsByClassName('titulo');
 const palabra = document.getElementsByClassName('palabra');
 
+const tiempoDOM = document.getElementById('tiempo');
+
+const startGame = document.getElementById('startGame');
+const reset = document.getElementById('reset');
+const mensaje = document.getElementById('mensaje');
+const mensajeFinal = document.getElementById('mensajeFinal');
+
 const erroresDOM = document.getElementById('errores');
 const intentosDOM = document.getElementById('intentos');
 
+const palabraDOM = document.getElementById('palabra');
 const wordToGuessDOM = document.getElementById('wordToGuess');
+
+let contador;
 
 
 // array de botones del documento
@@ -37,14 +47,41 @@ let arrPalabras = [
             "Suecia",
             "Albania",
             "Montenegro",
-            "Países Bajos"
+            "Paises Bajos"
         ]
     }
 ];
 
+// randomizar el opcUsuario
+// de momento es así, pero luego será con un prompt
+opcUsuario = Math.floor(Math.random() * arrPalabras.length);
+
 // seleccionar una palabra random después que el usuario haya elegido el tipo de palabra que quiere.
 
+// función que convierte una letra de la palabra random con tilde a una letra sin tilde
+// function convertirLetraTilde(letra) {
+//     switch (letra) {
+//         case "á":
+//             return "a";
+//         case "é":
+//             return "e";
+//         case "í":
+//             return "i";
+//         case "ó":
+//             return "o";
+//         case "ú":
+//             return "u";
+//         case "ü":
+//             return "u";
+//         default:
+//             return letra;
+//     }
+// }
+
 let wordToGuess = arrPalabras[opcUsuario].palabras[Math.floor(Math.random() * arrPalabras[opcUsuario].palabras.length)];
+
+// mostrar la palabra en la consola
+console.log("Palabra a averiguar: ", wordToGuess);
 
 // convertir la palabra a array
 wordToGuess = wordToGuess.split("");
@@ -60,7 +97,7 @@ let errores = 0;
 
 // hay que iterar los botones, porqué hay varios
 for (let i = 0; i < botones.length; i++) {
-   
+
     botones[i].addEventListener("click", (e) => {
         checkLetter(e.target.innerHTML.toLowerCase());
 
@@ -77,11 +114,11 @@ for (let i = 0; i < botones.length; i++) {
             e.target.setAttribute("disabled", "disabled");
         }
 
-        
+
         intentos++;
         intentosDOM.innerHTML = intentos;
-        
-        if(checkWord()) {
+
+        if (checkWord()) {
             console.log("Has ganado!");
             console.log(teclado);
 
@@ -94,7 +131,7 @@ for (let i = 0; i < botones.length; i++) {
             !e.target.classList.contains("seleccionada")
         ) {
             e.target.classList.toggle("seleccionada");
-            
+
         }
     });
 }
@@ -123,19 +160,85 @@ function checkLetter(letter) {
 function checkWord() {
     // recorrer el array de la palabra
     let palabraCompleta = true;
-    
+
     let palabrasDOM = wordToGuessDOM.children;
 
     // solo se puede iterar asi?
     for (let i = 0; i < palabrasDOM.length; i++) {
         if (palabrasDOM[i].innerHTML === "?") {
             palabraCompleta = false;
-            
         }
     }
-    
+
     if (palabraCompleta) {
+        clearInterval(contador);
+        console.log("Se acabó el tiempo.", contador);
         console.log("Palabra completa.");
         teclado.classList.add("d-none");
+
+        // mensaje de ganador
+        mensaje.classList.remove("d-none");
+        mensajeFinal.innerHTML = "Has ganado!";
+
     }
 }
+
+// función para que cuando el jugador empieze a jugar, se inicie un contador de tiempo
+
+function tiempo() {
+    let tiempo = 180;
+    let minutos = 0;
+    let segundos = 0;
+    contador = setInterval(() => {
+        tiempo= tiempo - 30;
+
+        segundos = tiempo % 60;
+        minutos = Math.floor(tiempo / 60);
+
+        if (tiempo < 60) {
+            tiempoDOM.classList.add("contador-peligro");
+        }
+
+        if (segundos < 10) {
+            segundos = "0" + segundos;
+        }
+
+        if (tiempo <= 0) {
+            clearInterval(contador);
+            mensaje.classList.remove("d-none");
+            mensajeFinal.innerHTML = "Se acabó el tiempo!";
+            mensajeFinal.classList.add("text-danger");
+            teclado.classList.add("d-none");
+            palabraDOM.classList.add("d-none");
+        }
+
+        tiempoDOM.innerHTML = minutos + ":" + segundos;
+    }, 1000);
+}
+
+// función para que cuando el jugador empieze a jugar, se inicie un contador de tiempo
+startGame.addEventListener("click", () => {
+    tiempo();
+    startGame.classList.add("d-none");
+    teclado.classList.remove("d-none");
+    palabraDOM.classList.remove("d-none");
+    wordToGuessDOM.classList.remove("d-none");
+});
+
+
+// Cambia el color de fondo del body cuando el mouse pasa por encima del botón
+startGame.addEventListener('mouseover', function () {
+    document.body.classList.add('hoverStartGame');
+});
+
+// Cambia el color de fondo del body de nuevo al original cuando el mouse sale del botón
+startGame.addEventListener('mouseout', function () {
+    document.body.classList.remove('hoverStartGame');
+});
+
+// función para resetear el juego
+reset.addEventListener("click", () => {
+    // volver a generar una palabra random
+    // de momento hará el reset de la página
+    location.reload();
+});
